4e9d42cbb52ee09456da49ed646c9200490fcc6e
GodLJonathan <91397110+GodLJonathan@users.noreply.github.com>
2021-10-18 19:02:37 +0530

    Update playright.js

--- playright.js
+++ playright.js
@@ -13,7 +13,7 @@ const download = async (url) => {
         await page.click('#login > div.auth-form-body.mt-3 > form > div.position-relative > input.btn.btn-primary.btn-block.js-sign-in-button');
         await page.goto(`${url}`);
         console.log("Forking the repo : ", url)
-        await page.click('//*[@id="repository-container-header"]/div[1]/ul/li[3]/form/button');
+        await page.click('//html/body/div[4]/div/main/div[1]/div[1]/ul/li[3]/form/button')
         await page.waitForTimeout(5000);
         console.log('Repo is Forked');
         browser.close();
